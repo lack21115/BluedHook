2e94013a0d69ddad3f111a4862af8c57b447457f
zogodo <742782908@qq.com>
2022-06-29 14:20:36 +0800

    up

--- app/src/main/java/me/lack/bluedhook/Main.java
+++ app/src/main/java/me/lack/bluedhook/Main.java
@@ -57,22 +57,22 @@ public class Main implements IXposedHookLoadPackage {
                         XposedHelpers.setObjectField(obj, "msgContent", flashPath);
                     }
 
-                    if ("".equals(msgContent.toString()) continue;
+                    if ("".equals(msgContent.toString())) continue;
 
                     if (msgType == 55) {
                         String key = "61C0A240C4AF5F16DA0738512255BA16";
                         if (msgContent.contains(key)) {
-                            String data = msgContent.split(key)
-                            XposedHelpers.setObjectField(obj, "msgContent", data[0])
-                            XposedHelpers.setShortField(obj, "msgType", Short.parseShort(data[1]))
+                            String data = msgContent.split(key);
+                            XposedHelpers.setObjectField(obj, "msgContent", data[0]);
+                            XposedHelpers.setShortField(obj, "msgType", Short.parseShort(data[1]));
                         } else if (msgContent.contains("blued-burn")) {
-                            XposedHelpers.setShortField(obj, "msgType", 24)
+                            XposedHelpers.setShortField(obj, "msgType", 24);
                         } else if (msgContent.contains("blued-chatfiles") && (msgContent.contains("jpg") || msgContent.contains("png"))) {
-                            XposedHelpers.setShortField(obj, "msgType", 2)
+                            XposedHelpers.setShortField(obj, "msgType", 2);
                         } else if (msgContent.contains("blued-chatfiles") && (msgContent.contains("mp3"))) {
-                            XposedHelpers.setShortField(obj, "msgType", 3)
+                            XposedHelpers.setShortField(obj, "msgType", 3);
                         } else {
-                            XposedHelpers.setShortField(obj, "msgType", 1)
+                            XposedHelpers.setShortField(obj, "msgType", 1);
                         }
                     }
                 }
